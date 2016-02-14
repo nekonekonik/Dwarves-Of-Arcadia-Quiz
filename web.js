9a26c541cdfb0b628b0fce31c7a2bcbe99cb8676
Nicholette <nekonekonik@gmail.com>
2016-02-13 21:19:47 -0800

    fix: server code

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var gzippo = require('gzippo');
   var express = require('express');
   var app = express();
   var logger = require('morgan');
-  
-  app.use(logger);
+
+  app.use(logger('dev'));
   app.use(gzippo.staticGzip("" + __dirname + "/dist"));
   app.listen(process.env.PORT || 5000);
