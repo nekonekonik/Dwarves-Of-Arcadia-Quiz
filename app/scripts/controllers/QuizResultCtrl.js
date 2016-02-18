2762ed820fe259e9309e497255f629795c104c5d
kangsoontan <tankangsoon@gmail.com>
2016-02-17 20:16:01 -0800

    add result images

--- app/scripts/controllers/QuizResultCtrl.js
+++ app/scripts/controllers/QuizResultCtrl.js
@@ -4,13 +4,14 @@ angular.module('dwarvesOfArcadiaQuizApp')
   .controller('QuizResultCtrl', function ($scope, quizProcessor) {
     function initialize() {
       var results = [
-        { personality: 'Vala Gravel-Axe', value: quizProcessor.brave() }, 
-        { personality: 'Tiernan Jewelbreaker', value: quizProcessor.business() }, 
-        { personality: 'Albanwr Chip-Moon', value: quizProcessor.religious() }, 
-        { personality: 'Kinnat Bismuthsmith', value: quizProcessor.intelligent() }
+        { personality: 'Vala Gravel-Axe', value: quizProcessor.brave(), dwarfType: 'brave' }, 
+        { personality: 'Tiernan Jewelbreaker', value: quizProcessor.business(), dwarfType: 'business' }, 
+        { personality: 'Albanwr Chip-Moon', value: quizProcessor.religious(), dwarfType: 'religious' }, 
+        { personality: 'Kinnat Bismuthsmith', value: quizProcessor.intelligent(), dwarfTpye: 'intelligent' }
       ];
 
       $scope.result = results.slice(0).sort(function(a, b) { return b.value - a.value; })[0].personality; 
+      $scope.dwarfType = results.slice(0).sort(function(a, b) { return b.value - a.value; })[0].dwarfType; 
       ga("send", "event", $scope.result);
     }
 
